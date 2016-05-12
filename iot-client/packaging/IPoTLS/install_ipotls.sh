8fb730fccbf8681578616bf3fcc580f07a16261c
John McBride <john.mcbride@asavie.com>
2016-05-12 18:36:58 +0100

    Echo test

---
+++ iot-client/packaging/IPoTLS/install_ipotls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Licensed under Simplified BSD License (see LICENSE)
+# Asavie IPoTLS installation script: install and set up the IPoTLS application for supported Linux distributions
+# using the package manager and Asavie repositories.
+
+set -e
+logfile="ipotls-install.log"
+
+echo "TESTING"
